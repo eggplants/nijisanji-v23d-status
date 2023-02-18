dd7adbcfcdb7df5cde2013fc08a05e5117e1d5d6
eggplants <w10776e8w@yahoo.co.jp>
2023-02-19 06:41:24 +0900

    fix

--- getinfo.sh
+++ getinfo.sh
@@ -1,81 +1,80 @@
 #!/bin/bash
 
-command -v curl jq >/dev/null || {
-  echo "install :curl, jq">&2
-  exit 1
-}
+set -eux
 
-echo | grep -P "" |& grep -q "grep: invalid option" && {
-  echo "Required grep impl is GNU. BSD is useless.">&2
+if ! command -v curl jq >/dev/null; then
+  echo "install: curl. jq">&2
   exit 1
-}
+fi
+
+if echo | grep -P "" |& grep -q "grep: invalid option" ; then
+  echo "need GNU grep instead of BSD one">&2
+  exit 1
+fi
 
 # Live2D 2.0
 curl -s 'https://wikiwiki.jp/nijisanji/衣装等まとめ' \
   | grep -m1 '第1次' \
-  | sed -nE 's/[^>]+>([^<]+)</\1'$'\\n/gp' \
-  | sed 's_^/td><td>_,'$'\\n_' \
-  | grep -A1 "^," \
-  | sed -E "/,|--/d" \
-  | sed 's/ギルザレンIII/ギルザレンⅢ/' \
+  | grep -oE '>[^(<0-9,第]+' \
+  | tr -d \> \
+  | sed '1,4d;s/ギルザレンIII/ギルザレンⅢ/' \
   > 2dv2
 
 # Live2D 3.0
 curl -s 'https://wikiwiki.jp/nijisanji/衣装等まとめ' \
   | grep -m2 '第1次' \
   | sed 1d \
-  | sed -nE 's/[^>]+>([^<]+)</\1'$'\\n/gp' \
-  | sed 's_^/td><td>_,'$'\\n_' \
-  | grep -A1 "^," \
-  | sed -E "/,|--/d" \
-  | sed 's/ギルザレンIII/ギルザレンⅢ/' \
+  | grep -oE '>[^(<0-9,第]+' \
+  | tr -d \> \
+  | sed '1,4d;s/ギルザレンIII/ギルザレンⅢ/' \
   > 2dv3
 
 # 3D
 curl -s 'https://wikiwiki.jp/nijisanji/3Dモデルまとめ' \
   | sed -n '/<a href="#recent_presentation">/,/<a href="#videos_3d">/p' \
-  | sed -nE '/<li><a href="#[A-Z]/s/^.*>([^<]+)<.*$/\1/p' \
+  | grep '</a></li>' \
+  | grep -oE '>[^<]+' \
+  | sed '1d;s/>//' \
   | sed 's/ //g;s/ギルザレンIII/ギルザレンⅢ/' \
   > 3d
 
 # Liver
-curl -s 'https://www.nijisanji.jp/members' \
-  | sed -nE 's_.*type="application/json">([^<]+)<.*_\1_p' \
-  | jq -r '[
-      .props.pageProps.livers[]
-      |select(.affiliation|index("にじさんじ"))
-      |{n:.name,s:.subscribe_orders}
-    ]|sort_by(.s)[]|.n' \
-  | grep -vE '[가-힣]|[a-z]| ' \
+curl -s 'https://wikiwiki.jp/nijisanji/公式ライバー' \
+  | sed -n '/<strong>公式ライバー一覧/,/star_members" title="VirtuaReal"/p' \
+  | grep -v 'background-color:#ddd' \
+  | sed -r '$d;s/.*title="([^"]+)".*/\1/' \
+  | grep -ivE '<|>|Nijisanji|出身|VirtuaReal|ライバー' \
+  | sed 's/&#23681;/岁/' \
+  | sort \
+  | uniq \
   > liver
 
 # Popularity by YouTube Subs
 echo name,popularity_rev > popular.csv
-curl -s 'https://wikiwiki.jp/nijisanji/メンバーデータ一覧%2Fチャンネル登録者数' \
-  | tr -d \\n \
-  | grep -oP '>デビュー日.*?adslot-h' \
-  | sed 's/left;">/\n/g' \
-  | sed '1d' \
-  | sed 's/^.*rel-wiki-page">//' \
-  | sed '/>(1st)<\/th>/d' \
+curl -s 'https://wikiwiki.jp/nijisanji/?cmd=popout&page=メンバーデータ一覧%2Fチャンネル登録者数&id=大台突破日' \
+  | sed -z 's/text-align:left;">/@/g' \
+  | tr @ \\n \
+  | sed -n '/月ノ美兎/,/レンタルWIKI by/p' \
+  | grep -E 'nijisanji.*title' \
+  | grep -vE '1st|切り抜き|ゲーム|ASMR' \
   | while read -r i
     do
       echo "$(
-        grep -oP '^.*?(?=<)' <<< "$i"
-      ) $(
-        grep -oP '>[0-9]+日<' <<< "$i" | wc -l
+        grep -oP '(?<=title=")[^"]+(?=")' <<< "$i"
+      ),$(
+        grep -oP 'center;">[0-9]+/[0-9]+/[0-9]+<' <<< "$i" | wc -l
       )"
     done \
-  | sed 's/ /,/g;s/ギルザレンIII/ギルザレンⅢ/' \
+  | sed 's/ギルザレンIII/ギルザレンⅢ/' \
   >> popular.csv
 
 # Synth data and output
 echo name,popularity,2dv2,2dv3,3d > result.csv
-for i in $(<liver)
+cat liver | while read i
 do
   echo "${i},$(
     sed 1d popular.csv \
-      | awk -F, -v name="${i}" '$1 == name{print 5*$2|"bc"}'
+      | awk -F, -v name="${i}" '$1 ~ name{print 5*$2|"bc"}'
   ),$(
     grep -q "$i" 2dv2 && echo o || echo x
   ),$(
@@ -84,7 +83,10 @@ do
     grep -q "$i" 3d && echo o || echo x
   )"
 done \
->> result.csv
+  | sed 's_,,_,-1,_' \
+  | sort -t, -rnk2 \
+  | sed 's_,-1,_,nd,_' \
+  >> result.csv
 
 # result: csv->json
 sed 1d result.csv | jq -Rsn '
@@ -94,7 +96,7 @@ sed 1d result.csv | jq -Rsn '
       | (.[] | select(length > 0) | ./",") as $i
       | {
         "name": $i[0],
-        "popularity": $i[1],
+        "popularity": (if $i[1] != "nd" then $i[1] else null end),
         "2dv2": $i[2],
         "2dv3": $i[3],
         "3d": $i[4]
